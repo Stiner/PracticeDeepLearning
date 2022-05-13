d157b1455c59c7532a6ebf1b9be63dec5c377f7b
Stiner <stiner01@gmail.com>
2022-05-13 07:52:48 +0000

    Initial commit

---
+++ funcs.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])      # x
+    w = np.array([0.5, 0.5])    # weight
+    b = -0.7                    # bias
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+def step_function(x):
+    return np.array(x > 0, int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def identity_function(x):
+    return x
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
