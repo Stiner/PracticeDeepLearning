a68ff7a1a48ceddde222d8c0a6f1bd43fbc71f20
Stiner <stiner01@gmail.com>
2022-06-26 13:00:17 +0900

    행렬 연습 코드 추가

---
+++ matrix_practice.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+if __name__ == "__main":
+    mat1 = np.array(
+        [
+            [2],
+            [1],
+            [6]
+        ]
+    )
+
+    mat2 = np.array(
+        [
+            [1,3,5,6,1,4,7,8,9,2,4,5]
+        ]
+    )
+
+    print(mat1.shape)
+    print(mat2.shape)
+
+    result = np.dot(mat1, mat2)
+    print(result.shape)
+    print(result)
