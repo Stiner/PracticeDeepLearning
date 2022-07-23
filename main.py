24124011c7bb746000cb9afb7fbeba74feffcff6
Stiner <stiner01@gmail.com>
2022-07-23 14:33:28 +0900

    이름 오류 수정

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#import practice_ANN as practice
+#import practice_neuralnet as practice
 #import practice_matrix as practice
-#import practice_mnist as practice
-import practice_ANN_mnist as practice
+import practice_mnist as practice
+#import practice_neuralnet_mnist as practice
 
 practice.main()
