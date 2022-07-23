6a80032b6f53f2ac800e12919bea4640c8194932
Stiner <stiner01@gmail.com>
2022-07-23 14:42:07 +0900

    3.6.2 신경망 추론 처리 연습

--- practice_neuralnet_mnist.py
+++ practice_neuralnet_mnist.py
@@ -1,18 +1,33 @@
 import sys, os
 sys.path.append(os.pardir)
 import numpy as np
+import pickle
 from dataset.mnist import load_mnist
-from PIL import Image
 from funcs import sigmoid
 from funcs import softmax
 
 def main():
-    return
+    x, t = get_data()
+    network = init_network()
 
-def get_data():
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y) # 확율이 가장 높은 원소의 인덱스를 얻는다.
+        if p == t[i]:
+            accuracy_cnt += 1
+
+    print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+def get_data(): # (시험 이미지, 시험 라벨)
     (x_train, t_train), (x_test, t_test) = \
-        load_mnist(normalize=True, flatten=True, one_hot_label=True)
+        load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
+
+def get_train(): # (훈련 이미지, 훈련 라벨)
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize=False, flatten=True, one_hot_label=False)
+    return x_train, t_train
 
 def init_network():
     with open("sample_weight.pkl", 'rb') as f:
