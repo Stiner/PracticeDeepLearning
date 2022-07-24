9ed949317dafe67b2ff647464ae23bceb6ceb8e3
Stiner <stiner01@gmail.com>
2022-07-24 13:59:02 +0900

    batching

--- practice_neuralnet_mnist.py
+++ practice_neuralnet_mnist.py
@@ -5,26 +5,43 @@ import pickle
 from dataset.mnist import load_mnist
 from funcs import sigmoid
 from funcs import softmax
+from funcs import img_show
 
 def main():
     x, t = get_data()
     network = init_network()
 
+    ## 이미지 뷰어로 보기
+    #idx = 9
+    #y = predict(network, x[idx])
+    #p = np.argmax(y)
+    #print(p)
+    #img = x[idx].reshape(28, 28)
+    #img = img * 255
+    #img_show(img)
+
     accuracy_cnt = 0
-    for i in range(len(x)):
-        y = predict(network, x[i])
-        p = np.argmax(y) # 확율이 가장 높은 원소의 인덱스를 얻는다.
-        if p == t[i]:
-            accuracy_cnt += 1
+    ## 이미지 1장을 계산
+    #for i in range(len(x)):
+    #    y = predict(network, x[i])
+    #    p = np.argmax(y) ## 확율이 가장 높은 원소의 인덱스를 얻는다.
+    #    if p == t[i]:
+    #        accuracy_cnt += 1
+
+    ## 이미지 10000장을 한번에 계산
+    y = predict(network, x) ## y.shape = (10000, 10)
+    for i in range(len(y)):
+        p = np.argmax(y[i]) ## 10개의 확율값 중 가장 높은값의 인덱스(이미지 내용이 인덱스의 값일 확율)
+        accuracy_cnt += np.sum(p == t[i])
 
     print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
 
-def get_data(): # (시험 이미지, 시험 라벨)
+def get_data(): ## (시험 이미지, 시험 라벨)
     (x_train, t_train), (x_test, t_test) = \
         load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
 
-def get_train(): # (훈련 이미지, 훈련 라벨)
+def get_train(): ## (훈련 이미지, 훈련 라벨)
     (x_train, t_train), (x_test, t_test) = \
         load_mnist(normalize=False, flatten=True, one_hot_label=False)
     return x_train, t_train
